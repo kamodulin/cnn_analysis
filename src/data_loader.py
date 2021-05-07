3ec46351dc8c8ee4a39f1c66ae3f377641211114
Kamran Ahmed <k.ahmed@berkeley.edu>
2021-05-07 02:55:12 -0400

    Update reference to dataroot

--- src/data_loader.py
+++ src/data_loader.py
@@ -9,7 +9,7 @@ def load_data():
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
-    validation_set = datasets.ImageNet(root="data/", split="val", transform=preprocess)
+    validation_set = datasets.ImageNet(root="../data/", split="val", transform=preprocess)
 
     loader = torch.utils.data.DataLoader(validation_set, batch_size=2000, shuffle=True, num_workers=2)
 
