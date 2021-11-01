864137a6330f0c1d8efb840aaf11b1ae5be19904
Kamran Ahmed <k.ahmed@berkeley.edu>
2021-11-01 18:09:23 -0400

    Cleanup experiment files and remove models

--- src/experiment.py
+++ src/experiment.py
@@ -53,7 +53,7 @@ class Manager:
 
     def init_files(self):
         basedir = "data/perturbation-experiments/"
-        
+
         if not os.path.exists(basedir):
             os.makedirs(basedir)
 
@@ -63,22 +63,25 @@ class Manager:
         with open(meta, "w") as f:
             for key, value in self.params.items():
                 f.write(f"{str(key)}: {str(value)}\n")
-        
+
         with open(csv, "w") as f:
             f.write("layer, level, fraction, repeat, accuracy\n")
-        
+
         return csv
 
     def run(self, model, data_loader, device):
         for i, expt in enumerate(self.expts):
-            
+
             accuracy = expt.run(model, data_loader, device)
 
             with open(self.csv, "a") as f:
-                f.write(f"{expt.layer}, {expt.level}, {expt.fraction}, {expt.repeat}, {accuracy}\n")
+                f.write(
+                    f"{expt.layer}, {expt.level}, {expt.fraction}, {expt.repeat}, {accuracy}\n"
+                )
 
             progress_bar((i + 1) / len(self.expts))
-    
+
+
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
@@ -86,22 +89,43 @@ if __name__ == "__main__":
     parser.add_argument("--model", default="alexnet", help="model")
     parser.add_argument("--dataset", default="imagenet", help="dataset")
     parser.add_argument("-b", "--batch-size", default=256, type=int)
-    parser.add_argument("--workers", default=15, type=int, help="number of data loading workers")
-    parser.add_argument("--layers", nargs="+", default="all", help="layers to perform knockout")
-    parser.add_argument("--analysis", nargs="+", default=["synapse", "node"], help="level of analysis")
-    parser.add_argument("--fraction", nargs="+", default=[x / 10 for x in range(0, 11)], type=float, help="knockout fraction")
-    parser.add_argument("--repeats", default=1, type=int, help="number of repeats")
+    parser.add_argument("--workers",
+                        default=15,
+                        type=int,
+                        help="number of data loading workers")
+    parser.add_argument("--layers",
+                        nargs="+",
+                        default="all",
+                        help="layers to perform knockout")
+    parser.add_argument("--analysis",
+                        nargs="+",
+                        default=["synapse", "node"],
+                        help="level of analysis")
+    parser.add_argument("--fraction",
+                        nargs="+",
+                        default=[x / 10 for x in range(0, 11)],
+                        type=float,
+                        help="knockout fraction")
+    parser.add_argument("--repeats",
+                        default=1,
+                        type=int,
+                        help="number of repeats")
 
     args = parser.parse_args()
 
     val_data = load_dataset(args.dataset.lower(), split="val")
-    val_loader = DataLoader(val_data, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
+    val_loader = DataLoader(val_data,
+                            batch_size=args.batch_size,
+                            shuffle=True,
+                            num_workers=args.workers)
     num_classes = len(val_data.classes)
-    
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = torchvision.models.__dict__[args.model](pretrained=True, num_classes=num_classes) # does not work for CIFAR100 yet
+    model = torchvision.models.__dict__[args.model](
+        pretrained=True,
+        num_classes=num_classes)  # does not work for CIFAR100 yet
     pretrained_weights = get_state_dict(model)
-    
+
     if args.layers == "all":
         layers = []
         for name, param in model.named_parameters():
@@ -118,14 +142,12 @@ if __name__ == "__main__":
             if weight not in named_params or bias not in named_params:
                 raise ValueError(f"{layer} is not a valid layer")
             else:
-                layers.append(layer) 
+                layers.append(layer)
 
-    params = OrderedDict(
-        layers = layers,
-        levels = args.analysis,
-        fraction = args.fraction,
-        repeats = range(args.repeats)
-    )
+    params = OrderedDict(layers=layers,
+                         levels=args.analysis,
+                         fraction=args.fraction,
+                         repeats=range(args.repeats))
 
     m = Manager(params)
     m.run(model, val_loader, device)
--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,15 @@ import sys
 
 from collections import OrderedDict
 
+
 def get_state_dict(model):
-    return OrderedDict([(name, tensor.clone()) for name, tensor in model.state_dict().items()])
+    return OrderedDict([(name, tensor.clone())
+                        for name, tensor in model.state_dict().items()])
+
 
 def set_state_dict(model, state_dict):
     model.load_state_dict(state_dict)
+
 
 def get_params(model, layer):
     state_dict = get_state_dict(model)
@@ -14,11 +18,13 @@ def get_params(model, layer):
     b = state_dict[layer + ".bias"].clone()
     return W, b
 
+
 def set_params(model, layer, W, b):
     state_dict = get_state_dict(model)
     state_dict[layer + ".weight"] = W
     state_dict[layer + ".bias"] = b
     set_state_dict(model, state_dict)
+
 
 def progress_bar(percent, **kwargs):
     bar_length = 40
@@ -27,10 +33,10 @@ def progress_bar(percent, **kwargs):
     y = bar_length - x
 
     bar = f"[{'=' * x}{' ' * y}] {percent*100:>3.0f}%"
-    
+
     for key, value in kwargs.items():
         bar += f" - {key}: {value:.3f}"
-    
+
     sys.stdout.write("\r")
     sys.stdout.write(bar)
     sys.stdout.flush()
